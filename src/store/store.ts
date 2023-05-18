c622b18b0c73b18b185296cfcad848b997864514
Mackewinsson P <mackewinsson@gmail.com>
2023-05-18 21:07:36 +0200

    config for redux added

---
+++ src/store/store.ts
@@ -0,0 +1,24 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { authSlice } from "./authSlice";
+import { createWrapper } from "next-redux-wrapper";
+import logger from "redux-logger";
+
+const makeStore = () =>
+  configureStore({
+    reducer: {
+      [authSlice.name]: authSlice.reducer,
+    },
+    devTools: true,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  });
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type AppState = ReturnType<AppStore["getState"]>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppState,
+  unknown,
+  Action
+>;
+
+export const wrapper = createWrapper<AppStore>(makeStore);
