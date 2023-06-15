7064579225f0c21b5a96bbe02ffed75d7107b092
Mackewinsson P <mackewinsson@gmail.com>
2023-06-15 23:06:07 +0200

    tailwind and shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Button } from "../../ src/components/button";
+
+export function ButtonDemo() {
+  return <Button>Button</Button>;
+}
 
 export default function Home() {
   return (
     <>
       <div>Home</div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ButtonDemo />
     </>
   );
 }
